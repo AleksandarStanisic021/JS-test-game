569dc2d3913e84e90ac5701d852eb931d866d67a
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-07 19:45:45 +0200

    simple grid

--- src/core/Game.js
+++ src/core/Game.js
@@ -1,7 +1,9 @@
+import { RenderSystem } from "./systems/RenderSystem";
 export class Game {
   constructor() {
     this.canvas = document.createElement("canvas");
-    this.ctx = this.canvas.getContext("2d");
+    //this.ctx = this.canvas.getContext("2d");
+    this.renderSystem = new RenderSystem(this.canvas);
     this.init();
   }
   init() {
@@ -15,12 +17,7 @@ export class Game {
     this.canvas.height = window.innerHeight - 20;
   }
   gameLoop(timestamp) {
-    this.render();
+    this.renderSystem.render();
     requestAnimationFrame((t) => this.gameLoop(t));
-  }
-  render() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillStyle = "#270330";
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
---
+++ src/core/systems/RenderSystem.js
@@ -0,0 +1,32 @@
+import { TILE_SIZE } from "../constants.js";
+
+export class RenderSystem {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.ctx.imageSmoothingEnabled = false;
+  }
+  render() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "#270330";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.renderGrid();
+  }
+
+  renderGrid() {
+    this.ctx.strokeStyle = "#3a0e5c";
+    this.ctx.lineWidth = 5;
+    for (let i = 0; i < this.canvas.width; i += TILE_SIZE) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(i, 0);
+      this.ctx.lineTo(i, this.canvas.height);
+      this.ctx.stroke();
+    }
+    for (let i = 0; i < this.canvas.height; i += TILE_SIZE) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, i);
+      this.ctx.lineTo(this.canvas.width, i);
+      this.ctx.stroke();
+    }
+  }
+}
